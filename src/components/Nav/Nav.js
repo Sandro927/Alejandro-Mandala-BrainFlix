9561f259a87223be3e45f6b06d90a2ac491619f2
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-02-23 14:09:08 -0500

    global styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Nav.scss'
+import logo from '../../assets/images/logo/BrainFlix-logo.svg'
+import avatar from '../../assets/images/images/Mohan-muruge.jpg'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <section className="nav">
+        <div className="nav__content">
+            <a className="nav__link" href='/'>
+              <img className="nav__logo" src={logo} alt='BrainFlix Logo' />
+            </a>
+
+            <div>
+              <form>
+                <input  type='text' placeholder='Search' />
+              </form>
+              <img className="nav__avatar" src={avatar} alt='Avatar'/>
+              <button>UPLOAD</button>
+            </div>
+          
+        </div>
+    </section>
   )
 }
 
