46e74d44dab0594da1b8beaab1234ccfbe9747c1
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-02-25 00:56:46 -0500

    created structure for next videos list

---
+++ src/components/VideoList/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function VideoList() {
+  return (
+    <section className="videos">
+        <div className="videos__header">
+            <h6 className="videos__title">NEXT VIDEOS</h6>
+            
+        </div>
+    </section>
+  )
+}
+
+export default VideoList
