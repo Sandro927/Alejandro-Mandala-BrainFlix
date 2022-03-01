2e4ae2b897f7a51eb1bb96d6e4e8770941d4c0c7
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-01 02:26:06 -0500

    installed axios to pull video data from API

--- src/components/ActiveVideo/ActiveVideo.js
+++ src/components/ActiveVideo/ActiveVideo.js
@@ -6,13 +6,28 @@ import NextVideoList from '../NextVideoList/NextVideoList'
 import Videos from '../../data/videos.json'
 import VideoData from '../../data/video-details.json'
 import './ActiveVideo.scss'
+import axios from 'axios';
+
+const API_KEY = "a10e75f8-75fb-4de7-857f-8aa90025dc69";
 
 export class ActiveVideo extends Component {
 
   state = {
-    videos: Videos,
+    videos: [],
     videoData: VideoData,
     activeVideoData: VideoData[0],
+  }
+
+  componentDidMount() {
+    axios.get(`https://project-2-api.herokuapp.com/videos?api_key=${API_KEY}`)
+    .then((response) => {
+      this.setState({
+        videos: response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
 
   changeActiveVideo = (id) => {
