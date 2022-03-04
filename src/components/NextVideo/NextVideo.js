2f327588bffd2b9455c53d1dcea2a27e8495928a
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-04 02:32:32 -0500

    converted Nextvideo class back into function

--- src/components/NextVideo/NextVideo.js
+++ src/components/NextVideo/NextVideo.js
@@ -2,25 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './NextVideo.scss'
 
-class NextVideo extends React.Component {
-
-  
-render() {
-    return (
-      <div className="video">
-          <div className="video__thumbnail">
-            <Link to={`/${this.props.video.id}`}>
-              <img className="video__image" src={this.props.video.image} alt='Video Thumbnail'/>
-              </Link>
-          </div>
-          
-          <div className="video__details">
-              <p className="video__title">{this.props.video.title}</p>
-              <p className="video__author">{this.props.video.channel}</p>
-          </div>
-      </div>
-    )
-  }
+function NextVideo(props) {
+  return (
+    <div className="video">
+        <div className="video__thumbnail">
+          <Link to={`/${props.video.id}`}>
+            <img className="video__image" src={props.video.image} alt='Video Thumbnail'/>
+            </Link>
+        </div>
+        <div className="video__details">
+            <p className="video__title">{props.video.title}</p>
+            <p className="video__author">{props.video.channel}</p>
+        </div>
+    </div>
+  )  
 }
 
 export default NextVideo
