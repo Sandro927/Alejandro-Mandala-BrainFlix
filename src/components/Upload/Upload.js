61e947c3670cc815178a98029d1c746b4854bd5b
Alejandro Mandala <47231162+Sandro927@users.noreply.github.com>
2022-03-04 12:48:21 -0500

    created vide upload page skeleton

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -3,7 +3,18 @@ import './Upload.scss'
 
 function Upload() {
   return (
-    <div>Upload</div>
+    <section className="upload">
+      <div className="upload__content">
+        <h1>Upload Video</h1>
+        <div className="upload__body">
+          <figure>IMAGE GOES HERE</figure>
+          <form>
+            <input type="text" placeholder="Add a title to your video"/>
+            <input type="text" placeholder="Add a description to your video"/>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
