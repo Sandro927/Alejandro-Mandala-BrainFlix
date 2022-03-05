eef219f30052216eac0ec34a57719ceafe16c9d8
Sandro927 <mandala.alejandro@gmail.com>
2022-03-05 10:23:11 -0500

    added form inputs and began to style at mobile

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import './UploadForm.scss'
 
 export class UploadForm extends Component {
   render() {
     return (
       <form className="form">
-          <h1>Upload Form</h1>
+          <label className="form__label" for="videoTitle">TITLE OF YOUR VIDEO</label>
+          <input className="form__text" type="text" name="videoTitle" />
+          
+          <label className="form__label" for="videoDescription">ADD A VIDEO DESCRIPTION</label>
+          <textarea className="form__textarea" name="videoDescription" rows="4" />
       </form>
     )
   }
