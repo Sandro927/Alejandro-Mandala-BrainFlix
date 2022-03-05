30fb6a17cf1a21928fbf58f80b3e62e539a9dbe6
Sandro927 <mandala.alejandro@gmail.com>
2022-03-05 10:30:05 -0500

    added styles to upload form at mobile

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -6,10 +6,10 @@ export class UploadForm extends Component {
     return (
       <form className="form">
           <label className="form__label" for="videoTitle">TITLE OF YOUR VIDEO</label>
-          <input className="form__text" type="text" name="videoTitle" />
+          <input className="form__text" type="text" name="videoTitle" placeholder="Add a title to your video"/>
           
           <label className="form__label" for="videoDescription">ADD A VIDEO DESCRIPTION</label>
-          <textarea className="form__textarea" name="videoDescription" rows="4" />
+          <textarea className="form__textarea" name="videoDescription" rows="4" placeholder="Add a description to your video"/>
       </form>
     )
   }
