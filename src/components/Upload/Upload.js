ec99696a18fd51db4225f32d20c6df2cbb5f0ab7
Sandro927 <mandala.alejandro@gmail.com>
2022-03-05 10:07:50 -0500

    added upload image and caption

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import UploadForm from '../UploadForm/UploadForm'
 import './Upload.scss'
+import thumbnail from '../../assets/images/images/Upload-video-preview.jpg'
 
 function Upload() {
   return (
     <section className="upload">
       <div className="upload__content">
-        <h1>Upload Video</h1>
+        <h1 className="upload__title">Upload Video</h1>
         <div className="upload__body">
-          <figure>IMAGE GOES HERE</figure>
+          <figure className="upload__figure">
+            <figcaption className="upload__caption">VIDEO THUMBNAIL</figcaption>
+            <img className="upload__image" src={thumbnail}></img>
+          </figure>
           <UploadForm />
         </div>
       </div>
